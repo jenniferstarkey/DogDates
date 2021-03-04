088c1d598450d4248eb8bad624f4438b6df59f69
jenniferstarkey <jenniferrstarkey@yahoo.com>
2021-03-03 18:17:55 -0600

    trying deploying

--- DogDates/Models/Comment.cs
+++ DogDates/Models/Comment.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DogDates.Models
 {
+    [Table("comment")]
+
     public class Comment
     {
         public int Id { get; set; }
--- DogDates/Models/UserProfile.cs
+++ DogDates/Models/UserProfile.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DogDates.Models
 {
+    [Table("userProfile")]
+
     public class UserProfile
     {
         public int Id { get; set; }
